7d7e8f7d8f4bf9de023a73eb085cb7cc5afb2251
liuran <liurann@outlook.com>
2025-09-01 10:00:10 +0800

    修改启动脚本

--- restartup.sh
+++ restartup.sh
@@ -5,9 +5,12 @@ git log -1
 
 pm2 delete StockApp
 
+npm run build
+
 export HOST=0.0.0.0
 export PORT=3000
 export DANGEROUSLY_DISABLE_HOST_CHECK=true
+export NODE_ENV=production
 
 pm2 start npm --name "StockApp" -- run start --prefix ~/Software/thinking-react
 
